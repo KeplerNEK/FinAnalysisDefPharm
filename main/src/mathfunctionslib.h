fe8edb2d43692a943db58ae749bf6db16c269fe7
KeplerNEK <neculau.cristinna01@gmail.com>
2026-03-07 17:17:34 +0100

    added for deque. will use deque for returns too.

--- main/src/mathfunctionslib.h
+++ main/src/mathfunctionslib.h
@@ -1,7 +1,13 @@
 #ifndef MATHFUNCTIONSLIB_H
 #define MATHFUNCTIONSLIB_H
-#include<vector>
 
-vector<double> log_returns(const vector<double>& prices);
+#include <vector>
+#include <deque>
+
+//historical log returns (uses vector, loops through all prices)
+std::vector<double> log_returnshistorical(const std::vector<double>& prices);
+
+//sliding-window log return (uses deque, last two prices only)
+double log_returns(const std::deque<double>& prices_window);
 
 #endif
